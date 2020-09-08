91266d980c4818e676d11311d71d5f94c2c7c029
branzaniovski <64470807+branzaniovski@users.noreply.github.com>
2020-09-08 11:16:53 +0300

    startin JS

---
+++ Js/udemy_js_course/section3/numbers.js
@@ -0,0 +1,16 @@
+let x = 11
+let num = (x + 1) * 2
+
+//console.log(num)
+
+let age = 19
+let dogYears = (age + 1) / 7
+//console.log(ogYears)
+
+//challenge area
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100 + '%'
+
+console.log(percent)
---
+++ Js/udemy_js_course/section3/strings.js
@@ -0,0 +1,14 @@
+let firtThing = 'stopper'
+let secondThing = 'enemy'
+
+let fullname = ' the ' + firtThing + ' is the ' + secondThing
+//console.log(fullname)
+
+
+//challenge area
+
+let country = 'Romania'
+let city = 'Cluj Napoca'
+let county = 'Cluj'
+let location = city + ' - ' + county + ', ' + country
+console.log(location)
